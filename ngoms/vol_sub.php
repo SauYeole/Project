b8f56353c2fd3858783441eae6f4ba6b0e75b0e6
Sudarshan Patil <patilsudarshan9975@gmail.com>
2025-05-01 12:34:35 +0530

    Update vol_sub.php

--- ngoms/vol_sub.php
+++ ngoms/vol_sub.php
@@ -1,34 +1,31 @@
 <?php 
+include 'include/conn.php';
 
-	include 'include/conn.php';
+if (isset($_POST['save'])) {
+    $vol_name = $_POST['vol_name'];
+    $vol_add = $_POST['vol_add'];
+    $vol_contact = $_POST['vol_contact'];
+    $vol_occu = $_POST['vol_occu'];
+    $vol_intr_wrk = $_POST['vol_intr_wrk'];
+    $doj_day = $_POST['doj_day'];
+    $doj_month = $_POST['doj_month'];
+    $doj_yr = $_POST['doj_yr'];
+    $dob_day = $_POST['dob_day'];
+    $dob_month = $_POST['dob_month'];
+    $dob_yr = $_POST['dob_yr'];
 
-	if (isset($_POST['save']))
-	{
-		$vol_name=$_POST['vol_name'];
-		$vol_add=$_POST['vol_add'];
-		$vol_contact=$_POST['vol_contact'];
-		$vol_occu=$_POST['vol_occu'];
-		$vol_intr_wrk=$_POST['vol_intr_wrk'];
-		$doj_day=$_POST['doj_day'];
-		$doj_month=$_POST['doj_month'];
-		$doj_yr=$_POST['doj_yr'];
-		$dob_day=$_POST['dob_day'];
-		$dob_month=$_POST['dob_month'];
-		$dob_yr=$_POST['dob_yr'];
+    $stmt = $conn->prepare("INSERT INTO add_vol (vol_name, vol_add, vol_contact, vol_occu, vol_intr_wrk, doj_day, doj_month, doj_yr, dob_day, dob_month, dob_yr) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssssss", $vol_name, $vol_add, $vol_contact, $vol_occu, $vol_intr_wrk, $doj_day, $doj_month, $doj_yr, $dob_day, $dob_month, $dob_yr);
 
-		$insert=mysql_query("INSERT INTO add_vol(vol_name,vol_add,vol_contact,vol_occu,vol_intr_wrk,doj_day,doj_month,doj_yr,dob_day,dob_month,dob_yr) VALUES('$vol_name','$vol_add','$vol_contact','$vol_occu','$vol_intr_wrk','$doj_day','$doj_month','$doj_yr','$dob_day','$dob_month','$dob_yr')");
+    if ($stmt->execute()) {
+        header("Location: vol_list.php");
+        exit;
+    } else {
+        echo "Failed: " . $stmt->error;
+    }
 
-		if ($insert)
-		{
-			header('location:vol_list.php');
-		}
-		else
-		{
-			echo "Failed";
-		}
-	}
-	else
-	{
-		echo "not clicked";
-	}
+    $stmt->close();
+} else {
+    echo "Form not submitted";
+}
 ?>
